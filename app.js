0c5a468c69961fe29bcbb089c7ed2953235fcf90
Sergey Germanov <sergyleo@yahoo.com>
2020-04-01 19:59:50 +1300

    MongoDb fully connected

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose
   })
   .then(() => console.log("MongoDB connected..."))
   .catch(err => console.log(err));
+mongoose.set("useFindAndModify", false);
 
 app.set("port", process.env.PORT || 8080);
 app.set("view engine", "ejs");
@@ -23,22 +24,22 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 // seedDb();
 
-var dataBase = {
-  79059847682: {
-    name: "Сергей Германов",
-    bonus: 1000,
-    money: 1000,
-    bonusIndex: 25,
-    percent: 10
-  },
-  79039109829: {
-    name: "Алексей Германов",
-    bonus: 3000,
-    money: 1000,
-    bonusIndex: 10,
-    percent: 25
-  }
-};
+// var dataBase = {
+//   79059847682: {
+//     name: "Сергей Германов",
+//     bonus: 1000,
+//     money: 1000,
+//     bonusIndex: 25,
+//     percent: 10
+//   },
+//   79039109829: {
+//     name: "Алексей Германов",
+//     bonus: 3000,
+//     money: 1000,
+//     bonusIndex: 10,
+//     percent: 25
+//   }
+// };
 
 var phNumber;
 var idPhone;
@@ -63,18 +64,6 @@ app.post("/", function(req, res) {
   });
 });
 
-// app.post("/", function(req, res) {
-//   phNumber = req.body.telNo;
-//   idPhone = phNumber.replace(/[^\d.]/g, "");
-
-//   if (dataBase[idPhone]) {
-//     data = dataBase[idPhone];
-//     // need to check it carefully
-//   } else {
-//   }
-//   res.redirect("/client");
-// });
-
 app.get("/client", function(req, res) {
   if (phNumber && data) {
     res.render("client", { phNumber: phNumber, data: data, message: message });
@@ -85,16 +74,16 @@ app.get("/client", function(req, res) {
 });
 
 app.post("/client", function(req, res) {
-  dataBase[idPhone].bonusIndex = Number(req.body.bonusIndex);
-  dataBase[idPhone].percent = Number(req.body.percent);
+  data.bonusIndex = Number(req.body.bonusIndex);
+  data.percent = Number(req.body.percent);
 
-  var bonus = Number(req.body.bonus) / dataBase[idPhone].bonusIndex;
+  var bonus = Number(req.body.bonus) / data.bonusIndex;
   var percent = (Number(req.body.bonus) * Number(req.body.percent)) / 100;
-  var bonusPercent = dataBase[idPhone].bonus - Math.round(percent);
+  var bonusPercent = data.bonus - Math.round(percent);
 
   if (req.body.action === "deposit") {
-    dataBase[idPhone].bonus += Math.round(bonus);
-    dataBase[idPhone].money += Number(req.body.bonus);
+    data.bonus += Math.round(bonus);
+    data.money += Number(req.body.bonus);
     message = {
       fullPay: req.body.bonus,
       bonus: Math.round(bonus),
@@ -102,17 +91,17 @@ app.post("/client", function(req, res) {
     };
   } else {
     if (bonusPercent < 0) {
-      dataBase[idPhone].money += Math.abs(bonusPercent);
+      data.money += Math.abs(bonusPercent);
       message = {
         fullPay: req.body.bonus,
-        bonus: dataBase[idPhone].bonus,
-        pay: req.body.bonus - dataBase[idPhone].bonus,
+        bonus: data.bonus,
+        pay: req.body.bonus - data.bonus,
         action: "withdraw"
       };
-      dataBase[idPhone].bonus = 0;
+      data.bonus = 0;
     } else {
-      dataBase[idPhone].bonus -= Math.round(percent);
-      dataBase[idPhone].money += Number(req.body.bonus) - Math.round(percent);
+      data.bonus -= Math.round(percent);
+      data.money += Number(req.body.bonus) - Math.round(percent);
       message = {
         fullPay: req.body.bonus,
         bonus: Math.round(percent),
@@ -122,7 +111,20 @@ app.post("/client", function(req, res) {
     }
   }
 
-  res.redirect("/client");
+  dataBaseM.findOneAndUpdate({ phone: idPhone }, data, function(err, update) {
+    if (err) {
+      message = {
+        error: err,
+        action: "error"
+      };
+      console.log(`Clent ${data.name} NOT updated`);
+      res.redirect("/client");
+    } else {
+      //redirect somewhere(show page)
+      console.log(`Clent ${data.name} updated`);
+      res.redirect("/client");
+    }
+  });
 });
 
 app.get("/create", function(req, res) {
@@ -135,8 +137,6 @@ app.post("/create", function(req, res) {
 
   delete req.body.telNo;
 
-  // var bonus = Math.floor(Number(req.body.bonus) / Number(req.body.bonusIndex));
-
   clientCreated = req.body;
   clientCreated.phone = Number(idPhone);
   clientCreated.money = Number(req.body.bonus);
@@ -146,31 +146,21 @@ app.post("/create", function(req, res) {
     Number(req.body.bonus) / Number(req.body.bonusIndex)
   );
 
-  console.log(req.body);
-  console.log(clientCreated);
-  console.log(clientCreated.phone);
-  console.log(idPhone);
-  // dataBase[idPhone] = req.body;
-  // dataBase[idPhone].money = Number(req.body.bonus);
-  // dataBase[idPhone].bonusIndex = Number(req.body.bonusIndex);
-  // dataBase[idPhone].percent = Number(req.body.percent);
-  // dataBase[idPhone].bonus = bonus;
-
-  dataBaseM.create(clientCreated, function(err, client) {
-    if (err) {
-      console.log(err);
-      res.redirect("/create");
-    } else {
-      console.log("added a client");
-      res.redirect("/");
-    }
-  });
-
-  // if (dataBase[idPhone]) {
-  //   res.redirect("/");
-  // } else {
-  //   res.redirect("/create");
-  // }
+  if (dataBaseM.exists({ phone: idPhone })) {
+    console.log(
+      `Клиент с номером +${idPhone} уже существует. Введите другой номер.`
+    );
+  } else {
+    dataBaseM.create(clientCreated, function(err, client) {
+      if (err) {
+        console.log(err);
+        res.redirect("/create");
+      } else {
+        console.log("added a client");
+        res.redirect("/create");
+      }
+    });
+  }
 });
 
 app.listen(app.get("port"), function() {
